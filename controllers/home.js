84972a8ac80dc7cec8e3eab0c41cb571bc176820
Zach-Trani <trani.zach@gmail.com>
2023-01-29 12:32:10 -0500

    Added controllers/home.js - getIndex created here, this is for the homepage. createItem also created here, this is for the add item feature.

---
+++ controllers/home.js
@@ -0,0 +1,33 @@
+const ItemList = require('../models/ItemList') // can now reference the schema
+
+// two methods: one to get homepage, and two to create item
+
+// when talking to DB need to use async/await functions
+module.exports = {
+    getIndex: async (req, res) => {
+        try {
+            const items = await ItemList.find() // go into the DB-->collection and bring out all data into this new variable: items
+            res.render("index.ejs", { itemList: items }); // render the homepage & send the list of items as an object over to ejs to list them out
+        } catch (err) {
+            if (err) return res.status(500).send(err); // sends the line of the error
+        }
+    },
+    createItem: async (req, res) => {
+        // reference schema, and pass item into schema
+        const newItem = new ItemList(
+            {
+                // submitting a form creates a req to the server. input variable located in ejs body
+                textinput: req.body.textinput,
+                numinput: req.body.numinput
+            }
+        )
+        try {
+            await newItem.save(); // save checks if textinput & numinput meet the requirements in the schema
+            console.log(newItem)
+            res.redirect("/"); // reload the homepage
+        } catch(err) {
+        if (err) return res.status(500).send(err);
+        res.redirect("/");
+        }
+    }
+}
